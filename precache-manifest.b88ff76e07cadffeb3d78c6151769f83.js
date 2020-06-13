bc1df06eb1ae94bc9f3f6b3fe7f273bc012e259f
Varenik Victor <kabalx47@gmail.com>
2020-06-13 14:10:54 +0300

    Updates

---
+++ precache-manifest.b88ff76e07cadffeb3d78c6151769f83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed232f11315a34f3cf735bf08bb399a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555c5d27a494987ea14a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c6c21b7eb591ae98a9d8",
+    "url": "/static/js/2.ea75e5a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea75e5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555c5d27a494987ea14a",
+    "url": "/static/js/main.1da7a246.chunk.js"
+  },
+  {
+    "revision": "d27d4c16e756a0028816",
+    "url": "/static/js/runtime-main.d9f12f9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
